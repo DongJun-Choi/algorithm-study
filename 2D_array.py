9b1b9d522721dffde8d9419fd0ca733dcaa98231
DongJun-Choi <76031811+DongJun-Choi@users.noreply.github.com>
2025-09-07 02:13:10 +0900

    Solve 10798

--- 2D_array.py
+++ 2D_array.py
@@ -25,3 +25,42 @@
 
 # for row in total:
 #     print(*row)
+
+
+# 2566
+# 9*9 2차원 배열에서 최댓값 찾기
+# 최댓값을 찾으면서 최댓값 갱신될때마다 위치랑 최댓값 값 증가
+
+# A_max = 0
+# max_locate = [0]*2
+
+# for i in range(9):
+#     test = list(map(int, input().split()))
+#     for j in range(9):
+#         if A_max <= test[j]:
+#             A_max = test[j]
+#             max_locate[0] = i+1
+#             max_locate[1] = j+1
+
+# print(A_max)
+# print(max_locate[0], max_locate[1])
+
+
+# 10798
+# 걍 5*5 행렬에서 행으로 읽는게 아니라 열로 읽는 문제인듯?
+# 한줄의 단어는 최대 15네
+# 특이점: 공백 없이 들어옴, 3줄안에 5개가 무조건 존재하는 것 아닐 수 있음
+
+# A = [['']*15 for _ in range(5)]
+
+# for i in range(5):
+#     test = list(input())
+#     for j in range(len(test)):
+#         A[i][j] = test[j]
+
+# for j in range(15):
+#     for i in range(5):
+#         if A[i][j] != '':
+#             print(A[i][j], end = '')
+
+
