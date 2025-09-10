c051ad2ddbd126c48a2bc9af200d5f2b72859856
DongJun-Choi <76031811+DongJun-Choi@users.noreply.github.com>
2025-09-10 23:29:10 +0900

    Solve 2501

--- Divisors_Multiples_Prime.py
+++ Divisors_Multiples_Prime.py
@@ -17,3 +17,21 @@
 #         answer.append("neither")
 
 # print("\n".join(answer))
+
+
+# 2501
+# N
+# K는 N의 약수들 중 K번째로 작은 수
+
+# N, K = map(int, input().split())
+
+# num = []
+
+# for i in range(N):
+#     if N % (i+1) == 0:
+#         num.append(i+1)
+
+# if K > len(num):
+#     print(0)
+# else:
+#     print(num[K-1])
