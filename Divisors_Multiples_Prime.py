b47406251296f22a788761431156413cd3abfdb5
DongJun-Choi <76031811+DongJun-Choi@users.noreply.github.com>
2025-09-10 23:24:16 +0900

    Solve 5086

---
+++ Divisors_Multiples_Prime.py
@@ -0,0 +1,19 @@
+# 5086
+# 첫번째 숫자가 2번째 수의 약수면 factor, 배수면 multiple 둘다 아니면 neither
+
+# c = True
+# answer = []
+
+# while c:
+#     a, b = map(int, input().split())
+#     if a == 0:
+#         c = False
+#         break
+#     if a < b and b % a == 0:
+#         answer.append("factor")
+#     elif b < a and a % b == 0:
+#         answer.append("multiple")
+#     else:
+#         answer.append("neither")
+
+# print("\n".join(answer))
