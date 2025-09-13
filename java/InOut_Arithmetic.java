fc5d27418111e4a034ac7bfd803be7c2dc981ee2
DongJun-Choi <76031811+DongJun-Choi@users.noreply.github.com>
2025-09-13 20:30:41 +0900

    Solve 1000

--- java/InOut_Arithmetic.java
+++ java/InOut_Arithmetic.java
@@ -1,13 +1,14 @@
-// 2557
 import java.util.Scanner;
 
 public class InOut_Arithmetic {
     public static void main(String[] args) {
         // 2557
-        // Scanner sc = new Scanner(System.in);
-        // String s = sc.nextLine();
-        // System.out.println(s);
+        // System.out.println("Hello World!");
 
-        
+        // 1000
+        // Scanner sc = new Scanner(System.in);
+        // int a = sc.nextInt();
+        // int b = sc.nextInt();
+        // System.out.println(a+b);
     }
 }
