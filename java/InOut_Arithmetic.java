520a03f155a93398e4a3bbdcf47e41975a0472d3
DongJun-Choi <76031811+DongJun-Choi@users.noreply.github.com>
2025-09-13 23:53:58 +0900

    Solve 7287

--- java/InOut_Arithmetic.java
+++ java/InOut_Arithmetic.java
@@ -6,24 +6,29 @@ public class InOut_Arithmetic {
         // 2557
         // System.out.println("Hello World!");
 
+
         // 1000
         // Scanner sc = new Scanner(System.in);
         // int a = sc.nextInt();
         // int b = sc.nextInt();
         // System.out.println(a+b);
 
+
         // 1008
         // 정수 21억 이하 int, 이상이면 long
         // 실수 소수점 6~7자리 이하 float, 이상이면 double
+
         // Scanner sc = new Scanner(System.in);
         // double a = sc.nextDouble();
         // double b = sc.nextDouble();
         // System.out.println(a/b);
 
+
         // 10926
         // Scanner sc = new Scanner(System.in);
         // String s = sc.nextLine();
         // System.out.println(s+"??!");
+
 
         // 18108
         // Scanner sc = new Scanner(System.in);
@@ -33,15 +38,24 @@ public class InOut_Arithmetic {
         // 10172
         // \를 문자열로 출력하려면 \\
         // "을 문자열로 출력하려면 \"
+
         // System.out.println("|\\_/|");
         // System.out.println("|q p|   /}");
         // System.out.println("( 0 )\"\"\"\\");
         // System.out.println("|\"^\"`    |");
         // System.out.println("||_/=\\\\__|");
 
+
         // 10699
         // java.time.LocalDate패키지 사용
+
         // LocalDate today = LocalDate.now();
         // System.out.println(today);
+
+
+        // 7287
+        // 크롤링하라는건가? 그냥 출력이었네
+        // System.out.println("90");
+        // System.out.println("chlehdwns82");
     }
 }
