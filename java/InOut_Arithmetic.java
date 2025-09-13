ec05e102a7e8cf16ed1dca181604d5f12ec12556
DongJun-Choi <76031811+DongJun-Choi@users.noreply.github.com>
2025-09-13 20:24:42 +0900

    Solve 2557

---
+++ java/InOut_Arithmetic.java
@@ -0,0 +1,13 @@
+// 2557
+import java.util.Scanner;
+
+public class InOut_Arithmetic {
+    public static void main(String[] args) {
+        // 2557
+        // Scanner sc = new Scanner(System.in);
+        // String s = sc.nextLine();
+        // System.out.println(s);
+
+        
+    }
+}
