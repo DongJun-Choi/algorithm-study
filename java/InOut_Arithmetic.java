5a60f66d24305ce145721c01d6cbf390b53192b8
DongJun-Choi <76031811+DongJun-Choi@users.noreply.github.com>
2025-09-13 20:36:34 +0900

    Solve 1008

--- java/InOut_Arithmetic.java
+++ java/InOut_Arithmetic.java
@@ -10,5 +10,13 @@ public class InOut_Arithmetic {
         // int a = sc.nextInt();
         // int b = sc.nextInt();
         // System.out.println(a+b);
+
+        // 1008
+        // 정수 21억 이하 int, 이상이면 long
+        // 실수 소수점 6~7자리 이하 float, 이상이면 double
+        // Scanner sc = new Scanner(System.in);
+        // double a = sc.nextDouble();
+        // double b = sc.nextDouble();
+        // System.out.println(a/b);
     }
 }
