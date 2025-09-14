f1848e300752b6a3f790cb9e7f5b057f98bb7375
DongJun-Choi <76031811+DongJun-Choi@users.noreply.github.com>
2025-09-14 20:20:13 +0900

    Solve 11021

--- java/Loop.java
+++ java/Loop.java
@@ -10,6 +10,23 @@ public class Loop {
         // }
 
 
-        
+        // 10872
+        // Scanner sc = new Scanner(System.in);
+        // int a = sc.nextInt();
+        // long total = 1;
+        // for (int i=a; i>=1; i--) {
+        //     total = total * i;
+        // }
+        // System.out.println(total);
+
+
+        // 11021
+        // Scanner sc = new Scanner(System.in);
+        // int T = sc.nextInt();
+        // for (int i=1; i<=T; i++) {
+        //     int A = sc.nextInt();
+        //     int B = sc.nextInt();
+        //     System.out.println("Case #"+i+": "+(A+B));
+        // }
     }
 }
