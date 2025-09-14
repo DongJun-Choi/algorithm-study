055698e83a22c21f72903603cd21a62cd315f5b1
DongJun-Choi <76031811+DongJun-Choi@users.noreply.github.com>
2025-09-15 00:12:42 +0900

    Solve 15552

--- java/Loop.java
+++ java/Loop.java
@@ -1,7 +1,11 @@
 import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
 
 public class Loop {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         // 2741
         // Scanner sc = new Scanner(System.in);
         // int a = sc.nextInt();
@@ -28,5 +32,32 @@ public class Loop {
         //     int B = sc.nextInt();
         //     System.out.println("Case #"+i+": "+(A+B));
         // }
+
+        
+        // 15552
+        // 빠르게 입출력 하는 방법
+        // BufferedReader 빠른 한줄 입력을 위해 사용하는 클래스
+            // 빠른 한줄 입력을 위해 한줄씩 변환하는 것이 아닌 한 줄을 가져와서 한번에 읽고 저장
+            // 이후에 버퍼에서 꺼내서 사용하니 입출력 횟수 줄어들어 훨씬 빨라짐
+        // InputStreamReader System.in을 문자 스트림으로 연결해주는 어댑터 클래스
+            // System.in은 바이트 스트림으로 1byte씩 읽으면서 byte단위로 다룸
+            // 문자 단위 스트림으로 바꾸기(= UTF-16기반 자바 문자로 변환) 
+        // IOException 입력 처리 중 발생할 수 있는 예외 타입
+            // BufferedReader.readLine()이 IOException 던질 수 있어서 처리 필요
+        // StringTokenizer 공백 기준으로 문자열을 빠르게 토큰 단위로 분리하는 유틸
+        // StringBuilder 여러 출력 결과를 메모리에 보관을 사용하기 위해 사용
+
+        // BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        // StringBuilder sb = new StringBuilder();
+
+        // int t = Integer.parseInt(br.readLine());
+        // for (int i = 0; i < t; i++) {
+        //     StringTokenizer st = new StringTokenizer(br.readLine());
+        //     int a = Integer.parseInt(st.nextToken());
+        //     int b = Integer.parseInt(st.nextToken());
+        //     sb.append(a + b).append('\n');
+        // }
+
+        // System.out.print(sb);
     }
 }
