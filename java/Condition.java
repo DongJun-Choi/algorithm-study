1f7a499eccdc22c6808b903d77e018b5f70b72cc
DongJun-Choi <76031811+DongJun-Choi@users.noreply.github.com>
2025-09-14 19:41:27 +0900

    Solve 2420

--- java/Condition.java
+++ java/Condition.java
@@ -13,5 +13,31 @@ public class Condition {
         // } else {
         //     System.out.println("==");
         // }
+
+
+        // 2420
+        // |N-M|값을 구하라
+        // 나는 if 조건문으로 사용하였으나 Math.abs 함수를 사용하면 절댓값 처리 가능
+
+        // Scanner sc = new Scanner(System.in);
+        // long a = sc.nextLong();
+        // long b = sc.nextLong();
+        // if (a < 0 && b < 0) {
+        //     if (-a > -b) {
+        //         System.out.println((-a) - (-b));
+        //     } else {
+        //         System.out.println((-b) - (-a));
+        //     }
+        // } else if (a < 0 && b > 0) {
+        //     System.out.println(-a + b);
+        // } else if (a > 0 && b < 0) {
+        //     System.out.println(a + -b);
+        // } else {
+        //     if (a > b) {
+        //         System.out.println(a - b);
+        //     } else {
+        //         System.out.println(b - a);
+        //     }
+        // }
     }
 }
