85684ed6ef6a9b467f2132296f019fc878234310
DongJun-Choi <76031811+DongJun-Choi@users.noreply.github.com>
2025-09-15 22:01:02 +0900

    Solve 2475

--- java/Function.java
+++ java/Function.java
@@ -4,15 +4,33 @@ public class Function {
     public static void main(String[] args) {
         // 15964
         // A, B가 들어오면 A@B로 계산 = (A+B)*(A-B)
-        
+
         // Scanner sc = new Scanner(System.in);
         // int A = sc.nextInt();
         // int B = sc.nextInt();
 
         // int result = customOperator(A, B);
         // System.out.println(result);
+
+
+        // 2475
+        // Scanner sc = new Scanner(System.in);
+        // int[] nums = new int[5];        
+        
+        // for (int i = 0; i < 5; i++) {
+        //     nums[i] = sc.nextInt();
+        // }
+
+        // System.out.println(checkOperator(nums));
     }
     public static long customOperator(int a, int b) {
         return (long)(a + b) * (a - b);
+    }
+    public static int checkOperator(int[] arr) {
+        int sum = 0;
+        for (int n : arr) {
+            sum += n * n;
+        }
+        return sum % 10;
     }
 }
