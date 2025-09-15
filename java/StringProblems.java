75e5ccb5f10378475e38bdc790358167f48381cf
DongJun-Choi <76031811+DongJun-Choi@users.noreply.github.com>
2025-09-15 21:28:07 +0900

    Solve 2744

---
+++ java/StringProblems.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class StringProblems {
+    public static void main(String[] args) {
+        // 11654
+        // nextLine()은 문자열로 받음
+        // 아스키코드로 변경하는 법 char문자 앞에 (int)
+        // Scanner sc = new Scanner(System.in);
+        // char alpabat = sc.next().charAt(0);
+        
+        // System.out.println((int) alpabat);
+
+
+        // 2743
+        // Scanner sc = new Scanner(System.in);
+        // String word = sc.nextLine();
+
+        // System.out.println(word.length());
+
+
+        // 2744
+        // isUpperCase()를 사용하면 대문자 확인, toUpperCase()을 사용하면 대문자로 변경
+        // Scanner sc = new Scanner(System.in);
+        // String word = sc.nextLine();
+        // String change = "";
+
+        // for (int i = 0; i < word.length(); i++) {
+        //     char c = word.charAt(i);
+
+        //     if (Character.isUpperCase(c)) {
+        //         change += Character.toLowerCase(c);
+        //     } else {
+        //         change += Character.toUpperCase(c);
+        //     }
+        // }
+
+        // System.out.println(change);
+    }
+}
