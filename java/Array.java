dce2ed7ff78117ab7fb1c5280f1ba96b0ff1a202
DongJun-Choi <76031811+DongJun-Choi@users.noreply.github.com>
2025-09-15 21:12:10 +0900

    Solve 2738

---
+++ java/Array.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+public class Array {
+    public static void main(String[] args) {
+        // 10871
+        // Scanner sc = new Scanner(System.in);
+        // int N = sc.nextInt();
+        // int X = sc.nextInt();
+        // int[] arr = new int[N];
+        // for (int i = 0; i < N; i++) {
+        //     arr[i] = sc.nextInt();
+        // }
+        // for (int i=0;i<N;i++) {
+        //     if (arr[i] < X) {
+        //         System.out.println(arr[i]);
+        //     }
+        // }
+
+
+        // 2738
+        // Scanner sc = new Scanner(System.in);
+
+        // int N = sc.nextInt();
+        // int M = sc.nextInt();
+
+        // int[][] arr1 = new int[N][M];
+        // int[][] arr2 = new int[N][M];
+
+        // for (int i=0;i<N;i++) {
+        //     for (int j=0;j<M;j++) {
+        //         arr1[i][j] = sc.nextInt();
+        //     }
+        // }
+        // for (int i=0;i<N;i++) {
+        //     for (int j=0;j<M;j++) {
+        //         arr2[i][j] = sc.nextInt();
+        //     }
+        // }
+
+        // for (int i=0;i<N;i++) {
+        //     for (int j=0;j<M;j++) {
+        //         System.out.print((arr1[i][j] + arr2[i][j]) + " ");
+        //     }
+        //     System.out.println();
+        // }
+    }
+}
