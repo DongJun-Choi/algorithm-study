97350974ee5ed2d00570818a071e9f84b6c12b7d
DongJun-Choi <76031811+DongJun-Choi@users.noreply.github.com>
2025-09-19 15:37:14 +0900

    Solve 2231

--- java/BruteForce.java
+++ java/BruteForce.java
@@ -6,28 +6,55 @@ public class BruteForce {
         // 2798
         // 첫줄에 카드 개수 N, 최고의 합 M
         // 둘째줄에 각 카드의 값
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        // BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
+        // StringTokenizer st = new StringTokenizer(br.readLine());
+        // int n = Integer.parseInt(st.nextToken());
+        // int m = Integer.parseInt(st.nextToken());
 
-        st = new StringTokenizer(br.readLine());
-        int[] arr = new int[n];
-        for (int i=0;i<n;i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
-        }
+        // st = new StringTokenizer(br.readLine());
+        // int[] arr = new int[n];
+        // for (int i=0;i<n;i++) {
+        //     arr[i] = Integer.parseInt(st.nextToken());
+        // }
 
-        int sum = 0;
-        int result = 0;
-        for (int i=0;i<n;i++) {
-            for (int j=i+1;j<n;j++) {
-                for (int k=j+1;k<n;k++) {
-                    sum = arr[i]+arr[j]+arr[k];
-                    if (sum <= m && result < sum) result = sum;
-                }
-            }
-        }
-        System.out.println(result);
+        // int sum = 0;
+        // int result = 0;
+        // for (int i=0;i<n;i++) {
+        //     for (int j=i+1;j<n;j++) {
+        //         for (int k=j+1;k<n;k++) {
+        //             sum = arr[i]+arr[j]+arr[k];
+        //             if (sum <= m && result < sum) result = sum;
+        //         }
+        //     }
+        // }
+        // System.out.println(result);
+
+
+        // 2231
+        // 자연수 N, 분해합은 245의 분해합은 256(=245+2+4+5)
+        // 이러면 245은 256의 생성자가 된다.
+        // BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        // StringTokenizer st = new StringTokenizer(br.readLine());
+        // int n = Integer.parseInt(st.nextToken());
+        // int answer = 0;
+
+        // for (int i = 0; i < n; i++) {
+        //     int sum = i;
+        //     int temp = i;
+
+        //     while (temp > 0) {
+        //         sum += temp % 10;
+        //         temp /= 10;
+        //     }
+
+        //     if (sum == n) {
+        //         answer = i;
+        //         break;
+        //     }
+        // }
+
+        // System.out.println(answer);
     }
 }
