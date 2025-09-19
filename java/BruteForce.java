34e53540d5a40098314166c32d4ac5d5b87d4945
DongJun-Choi <76031811+DongJun-Choi@users.noreply.github.com>
2025-09-19 15:53:32 +0900

    Solve 19532

--- java/BruteForce.java
+++ java/BruteForce.java
@@ -56,5 +56,28 @@ public class BruteForce {
         // }
 
         // System.out.println(answer);
+
+
+        // 19532
+        // 연립방정식 값을 찾아라
+        // BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        // StringTokenizer st = new StringTokenizer(br.readLine());
+        // int a = Integer.parseInt(st.nextToken());
+        // int b = Integer.parseInt(st.nextToken());
+        // int c = Integer.parseInt(st.nextToken());
+        // int d = Integer.parseInt(st.nextToken());
+        // int e = Integer.parseInt(st.nextToken());
+        // int f = Integer.parseInt(st.nextToken());
+
+        // for (int i=-999; i<=999; i++) {
+        //     for (int j=-999; j<=999; j++) {
+        //         if (a*i + b*j == c && d*i + e*j == f) {
+        //             System.out.println(i + " " + j);
+        //             return;
+        //         }
+        //     }
+        // }
+
     }
 }
