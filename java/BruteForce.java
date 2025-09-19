72a69eece3e90415eff805715dd9a54bfcc783c9
DongJun-Choi <76031811+DongJun-Choi@users.noreply.github.com>
2025-09-19 16:36:34 +0900

    Solve 1018

--- java/BruteForce.java
+++ java/BruteForce.java
@@ -79,5 +79,68 @@ public class BruteForce {
         //     }
         // }
 
+
+        // 1018
+        // M*N의 크기의 보드 이걸 잘라서 8*8 크기의 체스판 만들려고함
+        // 처음에 N과 M을 준다.
+        // BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        // StringTokenizer st = new StringTokenizer(br.readLine());
+        // int m = Integer.parseInt(st.nextToken());
+        // int n = Integer.parseInt(st.nextToken());
+        
+        // char[][] arr = new char[m][n];
+        // char[][] arrB = new char[8][8];
+        // char[][] arrW = new char[8][8];
+
+        // int countB;
+        // int countW;
+        // int count = Integer.MAX_VALUE;
+
+        // for (int i=0;i<m;i++) {
+        //     String line = br.readLine();
+        //     for (int j=0;j<n;j++) {
+        //         arr[i][j] = line.charAt(j);
+        //     }
+        // }
+
+        // // 비교할 판 2개 만들기
+        // for (int i=0;i<8;i++) {
+        //     for (int j=0;j<8;j++) {
+        //         if (i%2==0 && j%2==0) {
+        //             arrB[i][j] = 'W';
+        //             arrW[i][j] = 'B';
+        //         } else if (i%2==0 && j%2!=0) {
+        //             arrB[i][j] = 'B';
+        //             arrW[i][j] = 'W';
+        //         } else if (i%2!=0 && j%2==0) {
+        //             arrB[i][j] = 'B';
+        //             arrW[i][j] = 'W';
+        //         } else {
+        //             arrB[i][j] = 'W';
+        //             arrW[i][j] = 'B';
+        //         }
+        //     }
+        // }
+        
+        // for (int i=0;i<=m-8;i++) {
+        //     for (int j=0;j<=n-8;j++) {
+        //         countB = 0;
+        //         countW = 0;
+
+        //         // 판 색 색칠 수
+        //         for (int x=0;x<8;x++) {
+        //             for (int y=0;y<8;y++) {
+        //                 if (arr[x+i][y+j] != arrB[x][y]) countB+=1;
+        //                 if (arr[x+i][y+j] != arrW[x][y]) countW+=1;
+        //             }
+        //         }
+        //         if (Math.min(countW, countB) <= count) count = Math.min(countW, countB);
+        //     }
+        // }
+        // System.out.println(count);
+
+
+        // 
     }
 }
