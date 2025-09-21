9ae75e01b21e4e0d84375f6a7f41e5dba1657fe7
DongJun-Choi <76031811+DongJun-Choi@users.noreply.github.com>
2025-09-21 10:37:22 +0900

    Solve 2750

---
+++ java/Sort.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Sort {
+    public static void main(String[] args) throws IOException {
+        // 2750
+        // N개의 수 들어오면 오름차 순으로 정렬 후 출력
+        // 배열과 리스트 사용 차이, 배열은 입력의 크기가 정해져 있을때
+        // 리스트는 입력 개수가 가변적일 때
+        // Arrays.sort()가 배열 오름차순, 내림차순은 없어서 직접해야함
+        // 객체 배열은 내림차순 존재 Arrays.sort(arr, Comparator.reverseOrder());
+        // *기본 배열과 객체 배열 차이 : int[] 기본, Integer[] 객체=(Integer는 힙에 객체 생성되고 참조로 접근)
+        // list(리스트 선언명).sort()가 리스트 오름차순, 
+        // StringBuilder를 사용하면 출력이 더 빨라짐
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+
+        for (int i=0;i<n;i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        Arrays.sort(arr);
+
+        StringBuilder sb = new StringBuilder();
+        for (int num : arr) {
+            sb.append(num).append("\n");
+        }
+        System.out.println(sb);
+        
+    }
+}
