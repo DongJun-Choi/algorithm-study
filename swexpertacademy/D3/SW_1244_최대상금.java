0b2bc1e2f1aafe5e08fe9f9691b29efd57d5fc4d
DongJun-Choi <76031811+DongJun-Choi@users.noreply.github.com>
2025-11-13 10:12:02 +0900

    SW: 1244_최대상금

--- swexpertacademy/D3/SW_1244_최대상금.java
+++ swexpertacademy/D3/SW_1244_최대상금.java
@@ -1,8 +1,14 @@
 package D3;
 
+import java.util.HashSet;
 import java.util.Scanner;
 
 public class SW_1244_최대상금 {
+    static int maxValue;
+    static int changeCount;
+    static char[] numbers;
+    static HashSet<String> visited;
+
     public static void main(String[] args) {
         // 입력
         // 첫 줄에 전체 테스트 케이스의 수 입력(최대 10까지)
@@ -14,28 +20,51 @@ public class SW_1244_최대상금 {
         // 앞쪽의 작은 숫자랑 뒤쪽의 큰 숫자를 변경해야 가장 큰 이득을 본다.
         // 무조건 바꿔야하기 때문에 만약 앞보다 큰 숫자들이 없이 정렬되어있을 경우에는 가장 뒤에 있는 숫자 2개 변경해야함
         // 내가 했던 거(그리디알고리즘)로는 해결 불가능. 백트래킹 알고리즘 사용해야함.
+        // 최선의 수를 찾아야함
 
         Scanner sc = new Scanner(System.in);
         int c = sc.nextInt();
         
         for (int test_case=1; test_case<=c; test_case++) {
-            int num = sc.nextInt();
-            int change = sc.nextInt();
+            String numStr = sc.next();
+            changeCount = sc.nextInt();
+            numbers = numStr.toCharArray();
+            maxValue = 0;
 
-            String str = String.valueOf(num);
-            int[] arr = new int[str.length()];
+            visited = new HashSet<>();
 
-            for (int i=0; i<str.length(); i++) {
-                arr[i] = str.charAt(i) - '0';
-            }
+            dfs(0);
 
-            
-
-            for (int i : arr) {
-                System.out.print(i + " ");
-            }
+            System.out.println("#" + test_case + " " + maxValue);
         }
 
+    }
+
+    static void dfs(int depth) {
+        if (depth == changeCount) {
+            int value = Integer.parseInt(new String(numbers));
+            maxValue = Math.max(maxValue, value);
+            return;
+        }
+
+        String state = new String(numbers) + depth;
+        if (visited.contains(state)) return;
+        visited.add(state);
+
+        int len = numbers.length;
+        for (int i = 0; i < len - 1; i++) {
+            for (int j = i + 1; j < len; j++) {
+                swap(i, j);
+                dfs(depth + 1);
+                swap(i, j);
+            }
+        }
+    }
+
+    static void swap(int i, int j) {
+        char temp = numbers[i];
+        numbers[i] = numbers[j];
+        numbers[j] = temp;
     }
     
 }
