a65c5703cc17237ba830ae641823b1a08ab19166
DongJun-Choi <76031811+DongJun-Choi@users.noreply.github.com>
2026-03-06 15:48:02 +0900

    [D5] Title: 숫자 게임, Time: 92 ms, Memory: 27,776 KB -BaekjoonHub

--- SWEA/D5/7206. 숫자 게임/숫자 게임.java
+++ SWEA/D5/7206. 숫자 게임/숫자 게임.java
@@ -4,69 +4,65 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Solution {
-	static int[] dp;
+	
+	static boolean[] visited = new boolean[100000];
+	static int[] dp = new int[100000];
 	
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int t = Integer.parseInt(br.readLine());
 		StringBuilder sb = new StringBuilder();
-		
-		dp = new int[100000];
-		for (int i = 0; i < dp.length; i++) dp[i] = -1;
+		int t = Integer.parseInt(br.readLine());
 		
 		for(int tc=1; tc<=t; tc++) {
-			int n = Integer.parseInt(br.readLine());			
-//			N이라는 숫자를 자릿수를 기준으로 최대 5개가 나오는데 이 순열을 구해야 함.
+			int n = Integer.parseInt(br.readLine());
 			
-//			그 순열이 구해지면 그 순열만큼 돌려야 함.
+//			어떻게 풀더라
+//			순열을 구해야함. 어떤 순열? 그 수가 있다면 수를 끊는
+//			어떻게 판단함? 비트마스킹? 한 숫자의 자릿수가 있으면 그 자릿수 - 1의 비트를 가진 숫자
 			
-//			돌리는 도중에 N < 10 이면
-//			count 값이랑 max값 비교 해서 return
+//			가져서 뭐함? 그 숫자를 가지고 순열을 싹다 돌리기
+//			dp는 왜 사용함? 만약 n의 숫자가 이미 했던 숫자라면 그 숫자의 최솟값 저장해서 사용? 
+//			카운트 값은 어떻게 저장할까?
 			
-			int ans = solve(n);
-			
-			sb.append('#').append(tc).append(' ').append(ans).append("\n");
+			sb.append('#').append(tc).append(' ').append(solve(n)).append("\n");
 		}
 		
 		System.out.println(sb);
 	}
-	
-	
-	static int solve(int n) {
-        if (n < 10) return 0;
-        if (n < dp.length && dp[n] != -1) return dp[n];
 
-        String s = String.valueOf(n);
-        int len = s.length();
-
-        int best = 0;
-
-        // len-1개의 경계(자리 사이)를 끊을지 말지 선택
-        // mask==0 은 "아예 안 끊음"이라서 최소 2조각 조건에 위배 -> 제외
-        int total = 1 << (len - 1);
-        for (int mask = 1; mask < total; mask++) {
-            int product = 1;
-            int start = 0;
-
-            for (int i = 0; i < len - 1; i++) {
-                if ((mask & (1 << i)) != 0) { // i와 i+1 사이를 끊는다
-                    int part = Integer.parseInt(s.substring(start, i + 1));
-                    product *= part;
-                    start = i + 1;
-                }
-            }
-
-            // 마지막 조각
-            int last = Integer.parseInt(s.substring(start));
-            product *= last;
-
-            // 이번에 한 번 수행했으니 +1
-            best = Math.max(best, 1 + solve(product));
-        }
-
-        if (n < dp.length) dp[n] = best;
-        return best;
-    }
-	
-
+	static int solve(int num) {
+//		자릿수를 알기 위해서는?
+		if(num < 10) return 0;
+		
+		if (visited[num]) return dp[num];
+        visited[num] = true;
+		
+		String s = String.valueOf(num);
+		int len = s.length();
+		int max = 0;
+		
+//		i가 1씩 증가하니까 순열로 사용
+//		(ex. num이 1234라면 2^2(자릿수-2?) = 4(100) => 1 | 2 | 3 | 4)
+//		(ex. num이 12345라면 2^3(자릿수-2?) = 8(1000) => 1 | 2 | 3 | 4 | 5)
+//		(ex. num이 12라면 2^0(자릿수-2?) = 1 => 1 | 2)
+//		(ex. num이 123라면 2^1(자릿수-2?) = 2(10) => 1 | 2 | 3)
+		for(int mask=1; mask < (1 << (len-1)); mask++) {
+			long product = 1;
+			int cur = 0;
+			
+			for(int i=0; i<len; i++) {
+				cur = cur * 10 + (s.charAt(i) - '0');
+				
+				if(i == len-1 || (mask & (1 << i)) != 0) {
+					product *= cur;
+					cur = 0;
+				}
+			}
+			
+			max = Math.max(max, 1+solve((int) product));			
+		}
+		
+		dp[num] = max;		
+		return dp[num];
+	}
 }
