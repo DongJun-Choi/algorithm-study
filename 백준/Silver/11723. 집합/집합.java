2f39b90c6b8f6f684dd7c6dcd2471655e1caa786
DongJun-Choi <76031811+DongJun-Choi@users.noreply.github.com>
2026-03-24 09:30:14 +0900

    [Silver V] Title: 집합, Time: 1068 ms, Memory: 313772 KB -BaekjoonHub

---
+++ 백준/Silver/11723. 집합/집합.java
@@ -0,0 +1,64 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.StringTokenizer;
+
+public class Main {
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		
+		int m = Integer.parseInt(br.readLine());
+		
+		Set<Integer> set = new HashSet<>();
+		
+		for(int i=0; i<m; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			String s = st.nextToken();
+			int num;
+			switch (s) {
+			case "add":
+				num = Integer.parseInt(st.nextToken());
+				set.add(num);
+				break;
+			case "remove":
+				num = Integer.parseInt(st.nextToken());
+				set.remove(num);
+				break;
+			case "check":
+				num = Integer.parseInt(st.nextToken());
+				if(set.contains(num)) {
+					sb.append(1).append("\n");
+				} else {
+					sb.append(0).append("\n");
+				}
+				break;
+			case "toggle":
+				num = Integer.parseInt(st.nextToken());
+				if(set.contains(num)) {
+					set.remove(num);
+				} else {
+					set.add(num);
+				}
+				break;
+			case "all":
+				for(int j=1; j<=20; j++) {
+					set.add(j);
+				}
+				break;
+			case "empty":
+				set.removeAll(set);
+				break;
+			}			
+			
+		}
+		
+		System.out.println(sb);
+		
+		
+	}
+
+}
