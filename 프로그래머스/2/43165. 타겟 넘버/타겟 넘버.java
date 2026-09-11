2afeaad044451c40e9e56369f65a9fed2a99395e
DongJun-Choi <76031811+DongJun-Choi@users.noreply.github.com>
2026-09-11 23:14:44 +0900

    [level 2] Title: 타겟 넘버, Time: 4.47 ms, Memory: 74 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,35 +1,29 @@
-import java.util.*;
-
 class Solution {
-    static int[] arr;
-    static int count, len, n;
+    
+    boolean[] visited;
+    int n;
+    int cnt;
     
     public int solution(int[] numbers, int target) {
-        int answer = 0;
+        n = numbers.length;
+        visited = new boolean[n];
+        cnt = 0;
         
-//         target의 수가 되게 하는 방법의 수
+        bfs(0, 0, numbers, target);
         
-        len = numbers.length;
-        n = target;
-        
-        arr = Arrays.copyOf(numbers, len);
-        
-        count = 0;
-        
-        dfs(0, 0);
-        
-        answer = count;
-        
+        int answer = cnt;
         return answer;
     }
     
-    static void dfs(int depth, int num) {
-        if(depth == len) {
-            if(num == n) count++;
+    void bfs(int num, int depth, int[] numbers, int target) {
+        if(depth == n) {
+            if(num == target) cnt++;
             return;
         }
         
-        dfs(depth+1, num+arr[depth]);
-        dfs(depth+1, num-arr[depth]);
+        bfs(num+numbers[depth], depth+1, numbers, target);
+        
+        bfs(num-numbers[depth], depth+1, numbers, target);
+        
     }
 }
