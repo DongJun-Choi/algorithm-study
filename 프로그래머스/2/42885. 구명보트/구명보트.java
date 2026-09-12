dce4ab4075cbc51f2569361f58e253852a7b5c37
DongJun-Choi <76031811+DongJun-Choi@users.noreply.github.com>
2026-09-12 12:53:49 +0900

    [level 2] Title: 구명보트, Time: 11.24 ms, Memory: 59.2 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.java
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -2,23 +2,27 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] people, int limit) {
-        
         Arrays.sort(people);
+        int cnt = 0;
+        int min_count = 0;
+        int n = people.length;
+        boolean[] visited = new boolean[n];
         
-        int left = 0;
-        int right = people.length - 1;
-        int boat = 0;
-        
-        while(left <= right) {
+        for(int i=n-1; i>=0; i--) {
+            if(visited[i]) continue;
             
-            if(people[left] + people[right] <= limit) {
-                left++;
+            if(people[i] + people[min_count] <= limit) {
+                visited[i] = true;
+                visited[min_count] = true;
+                cnt++;
+                min_count++;
+            } else {
+                visited[i] = true;
+                cnt++;
             }
-            
-            right--;
-            boat++;
         }
         
-        return boat;
+        int answer = cnt;
+        return answer;
     }
 }
